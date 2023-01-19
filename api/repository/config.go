0699febc516e522985f5cec1a056cba57e79ecac
SaraBroad <saraebroad@gmail.com>
2023-01-18 20:42:02 -0500

    switch db config

--- api/repository/config.go
+++ api/repository/config.go
@@ -2,11 +2,11 @@ package repository
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/SaraBroad/go-itinerary/api/models"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	// "gorm.io/gorm"
 )
 
 type Auth struct {
@@ -22,10 +22,14 @@ type Auth struct {
 
 func InitDatabase(auth *Auth) *gorm.DB {
 	dbUrl := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s", auth.Host, auth.Username, auth.Password, auth.DBName, auth.Port, auth.SSL, auth.Timezone)
-	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
+	//db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
+	db, err := gorm.Open(dbUrl)
 	if err != nil {
-		log.Fatalln(err)
+		// log.Fatalln(err)
+		panic(err.Error())
 	}
+	defer db.Close()
+
 	db.AutoMigrate(&models.Itinerary{}, &models.ItineraryItem{}, &models.DayNumber{}, &models.Category{}, &models.Price{}, &models.Location{})
 	return db
 }
