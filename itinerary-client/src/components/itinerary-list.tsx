5a5d7bfe94fd0cd6495b6ab30e1ca5c6efb9c84a
SaraBroad <saraebroad@gmail.com>
2023-10-24 20:56:21 -0400

    more react

--- itinerary-client/src/components/itinerary-list.tsx
+++ itinerary-client/src/components/itinerary-list.tsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import Itinerary from '../types'
 
-interface Props {
-    itinerary: Itinerary
+interface ListProps {
+    itineraries: Itinerary[]
 }
 
 const Itineraries: Itinerary[] = [
     {
-        id: "",
-        name: "",
-        items: [],
-        startDate: "",
-        endDate: "",
+        id: "12345",
+        name: "Summer 2022",
+        items: ['ice cream', 'mini golf', 'fishing'],
+        startDate: "07/01/2022",
+        endDate: "07/11/2022",
+        locations: ['Stone Harbor, NJ'],
+    },
+    {
+        id: "54321",
+        name: "Summer 2023",
+        items: ['', '', ''],
+        startDate: "08/01/2021",
+        endDate: "08/15/2021",
         locations: [],
     }
 ]
 
 export const ItineraryList = () => {
 
-// export const ItineraryList: React.FC<Props> = ({ itinerary }) => {
-    // console.log(itinerary)
-    return <div>Hi</div>
+// export const ItineraryList = ({ itinerary }: ListProps ) => {
+    return <div>List goes here</div>
 }
