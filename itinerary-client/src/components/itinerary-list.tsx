5f7ee906486de236fb234deff20edfcc6fa486f8
SaraBroad <saraebroad@gmail.com>
2023-10-27 14:36:41 -0400

    add material ui

--- itinerary-client/src/components/itinerary-list.tsx
+++ itinerary-client/src/components/itinerary-list.tsx
@@ -1,4 +1,11 @@
 import React, { useState } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import Itinerary from "../types";
 
 interface ListProps {
@@ -38,12 +45,18 @@ export const ItineraryList = () => {
   // export const ItineraryList = ({ itinerary }: ListProps ) => {
   return (
     <div>
+        <TableContainer component={Paper}>
+
+
+        </TableContainer>
       {itineraryList.map((i) => {
         return (
           <div>
             <div>
               <h3>Name: {i.name}</h3>{" "}
             </div>
+            <div>{i.startDate}</div>
+            <div>{i.endDate}</div>
             <div>Activities: {i.items}</div>
             {i.locations?.length === 0 ? (
               <div>Locations: No destinations yet</div>
--- itinerary-client/src/types.tsx
+++ itinerary-client/src/types.tsx
@@ -1,8 +1,8 @@
 export default interface Itinerary {
     id: string
     name: string
-    items: string[]
-    startDate: string
-    endDate: string
+    items?: string[]
+    startDate?: string
+    endDate?: string
     locations?: string[]
 }
