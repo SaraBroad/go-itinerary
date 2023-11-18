3c9ade8338cd7f2c2f714dbf33c4d88c9a4d8e87
SaraBroad <saraebroad@gmail.com>
2023-11-17 21:12:34 -0500

    update form

--- itinerary-client/src/components/ItineraryForm/ItineraryForm.tsx
+++ itinerary-client/src/components/ItineraryForm/ItineraryForm.tsx
@@ -2,58 +2,80 @@ import React, { ReactHTMLElement, useCallback, useState } from "react";
 import Button from "@mui/material/Button";
 
 export const ItineraryForm = () => {
-  const [name, setName] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [locations, setLocations] = useState<string[]>([]);
-
-  const [formData, setFormData] = useState({
+  const [values, setValues] = useState({
     name: "",
     startDate: "",
     endDate: "",
     locations: [],
+    // add isCompleted
   });
 
-  const handleLocationChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setLocations([...locations, value]);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
-    alert("Form Submitted");
-    console.log(e);
+    e.preventDefault();
+    console.log(values);
+    alert(JSON.stringify(values, null, 2));
   };
 
   return (
     <>
-    <div className="form-container">
-      <form onSubmit={handleSubmit} className="form-fields"> 
-        <label>
-          Name{": "}
-          <input value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <label>
-          StartDate{": "}
-          <input
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-          />
-        </label>
-        <label>
-          End Date{": "}
-          <input value={endDate} onChange={(e) => setEndDate(e.target.value)} />
-        </label>
-        <label>
-          Locations{": "}
-          <input value={locations} onChange={handleLocationChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-
-      {/* <Button variant="contained" onClick={handleSubmit}>
-        Submit
-      </Button> */}
-        </div>
+      <div>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="formGroup">
+            <label className="formLabel">Itinerary Name:</label>
+            <input
+              className="formField"
+              type="text"
+              name="name"
+              placeholder="Itinerary Name"
+              value={values.name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="formGroup">
+            <label className="formLabel">Start Date:</label>
+            <input
+              className="formField"
+              type="text"
+              name="startDate"
+              placeholder="Start Date"
+              value={values.startDate}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="formGroup">
+            <label className="formLabel">End Date:</label>
+            <input
+              className="formField"
+              type="text"
+              name="endDate"
+              placeholder="End Date"
+              value={values.endDate}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="formGroup">
+            <label className="formLabel">Locations:</label>
+            <input
+              className="formField"
+              type="text"
+              name="locations"
+              placeholder="Enter a list of locations"
+              value={values.locations}
+              onChange={handleChange}
+            />
+          </div>
+          <button className="formSubmitBtn" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 };
